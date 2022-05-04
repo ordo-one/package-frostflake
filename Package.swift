36eaf61fb5214ac80743d931e0123664e7290995
Joakim Hassila <jocke@ordo.one>
2022-05-04 08:10:19 +0200

    Remove not needed dependencies, add Frostflake library target.

--- Package.swift
+++ Package.swift
@@ -6,16 +6,22 @@ import PackageDescription
 let package = Package(
     name: "swift-frostflake",
     platforms: [.macOS(.v10_15)],
+    products: [
+      .library(
+        name: "Frostflake",
+        targets: ["Frostflake"]
+      ),
+      .executable(
+        name: "SwiftFrostflake",
+        targets: ["SwiftFrostflake"]
+      ),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/apple/swift-log", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/apple/swift-system", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-metrics", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "1.0.0-alpha"),
     ],
-
     targets: [
         .executableTarget(
             name: "SwiftFrostflake",
@@ -23,13 +29,9 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "SystemPackage", package: "swift-system"),
-                .product(name: "NIOCore", package: "swift-nio"),
-                .product(name: "NIOPosix", package: "swift-nio"),
-                .product(name: "Metrics", package: "swift-metrics"),
-                .product(name: "Lifecycle", package: "swift-service-lifecycle"),
-                .product(name: "LifecycleNIOCompat", package: "swift-service-lifecycle"),
             ]
         ),
+        .target(name: "Frostflake", path: "Sources/Frostflake"),
         .testTarget(
             name: "SwiftFrostflakeTests",
             dependencies: ["SwiftFrostflake"]
