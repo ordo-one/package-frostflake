8f746d5133980fd0897fad8bbc01a89671c3c113
Joakim Hassila <jocke@ordo.one>
2022-08-26 18:20:57 +0200

    Next iteration WIP

--- Benchmarks/Benchmark/FrostflakeBenchmark.swift
+++ Benchmarks/Benchmark/FrostflakeBenchmark.swift
@@ -14,15 +14,12 @@ let classIterationCount = 1000
 
 @_dynamicReplacement(for: registerBenchmarks)
 func benchmarks() {
-    print("Hello 1")
-  fflush(nil)
+
     // Once during runtime setup can be done before registering benchmarks
+/* Can't run this benchmark due to max number of frostflakes per second!
     let frostflake = Frostflake(generatorIdentifier: 0)
-    print("Hello 2")
-    fflush(nil)
     Frostflake.setup(sharedGenerator: frostflake)
-    print("Hello 3")
-    fflush(nil)
+
 
     Benchmark("Frostflake shared generator",
               metrics: [.cpu, .memory, .syscalls, .threads],
@@ -30,24 +27,14 @@ func benchmarks() {
               minimumRuntime: 100,
               disabled: false) {  benchmark in
         benchmark.measure {
-            for _ in 0 ..< 10 {
-                print("Hello 4")
-                fflush(nil)
-                        blackHole(Frostflake.generate())
-                print("Hello 5")
-                fflush(nil)
+            for _ in 0 ..< 1_000_000 {
+                blackHole(Frostflake.generate())
             }
         }
-        /*
-         benchmark.measure {
-         for _ in 0 ..< classIterationCount {
-         blackHole(Frostflake.generate())
-         }
-         }*/
     }
-
+*/
     Benchmark("Frostflake with locks") { benchmark in
-        let frostflakeFactory = Frostflake(generatorIdentifier: UInt16.random(in: 0...1000),
+        let frostflakeFactory = Frostflake(generatorIdentifier: UInt16.random(in: 0...(1<<generatorIdentifierBits)-1),
                                            concurrentAccess: true)
 
         benchmark.measure {
@@ -58,7 +45,7 @@ func benchmarks() {
     }
 
     Benchmark("Frostflake without locks") { benchmark in
-        let frostflakeFactory = Frostflake(generatorIdentifier: UInt16.random(in: 0...1000),
+        let frostflakeFactory = Frostflake(generatorIdentifier: UInt16.random(in: 0...(1<<generatorIdentifierBits)-1),
                                            concurrentAccess: false)
 
         benchmark.measure {
@@ -72,7 +59,7 @@ func benchmarks() {
 
         benchmark.measure {
             for _ in 0 ..< classIterationCount {
-                let frostflakeFactory = Frostflake(generatorIdentifier: UInt16.random(in: 0...1000),
+                let frostflakeFactory = Frostflake(generatorIdentifier: UInt16.random(in: 0...(1<<generatorIdentifierBits)-1),
                                                    concurrentAccess: true)
                 blackHole(frostflakeFactory)
             }
