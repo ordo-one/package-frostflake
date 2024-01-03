0ffcf2d5ba947e2e41998528a178e79a8b907a07
mr-swifter <103502437+mr-swifter@users.noreply.github.com>
2024-01-03 12:44:33 +0000

    remove commented dependencies

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -11,8 +11,6 @@ let package = Package(
     dependencies: [
         .package(path: "../"),
         .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.13.0"),
-        // .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        // .package(url: "https://github.com/apple/swift-system", from: "1.0.0")
     ],
     targets: [
         .executableTarget(
@@ -21,8 +19,6 @@ let package = Package(
                 .product(name: "FrostflakeKit", package: "package-frostflake"),
                 .product(name: "Benchmark", package: "package-benchmark"),
                 .product(name: "BenchmarkPlugin", package: "package-benchmark"),
-                // .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                // .product(name: "SystemPackage", package: "swift-system"),
             ],
             path: "Benchmarks/Frostflake"
         )
