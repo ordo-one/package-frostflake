d1425d8e0e91f7f9128d195e01c34f6de073a98e
Tim <0xtimc@gmail.com>
2026-02-18 09:55:09 +0100

    Bump benchmark manifest

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,13 +6,11 @@ import PackageDescription
 let package = Package(
     name: "Benchmarks",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v15)
     ],
     dependencies: [
         .package(path: "../"),
-        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.13.0")
-        // .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        // .package(url: "https://github.com/apple/swift-system", from: "1.0.0")
+        .package(url: "https://github.com/ordo-one/package-benchmark.git", from: "1.13.0"),
     ],
     targets: [
         .executableTarget(
@@ -20,9 +18,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Frostflake", package: "package-frostflake"),
                 .product(name: "Benchmark", package: "package-benchmark"),
-                .product(name: "BenchmarkPlugin", package: "package-benchmark")
-                // .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                // .product(name: "SystemPackage", package: "swift-system"),
+                .product(name: "BenchmarkPlugin", package: "package-benchmark"),
             ],
             path: "Benchmarks/Frostflake"
         )
